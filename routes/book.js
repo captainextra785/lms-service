08f291230133e795ee9c771b2181150ff163b487
captainmukesh <mukesh@richpanel.com>
2024-02-25 18:45:21 +0530

    Added book adding api

--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
 const bookRouter = express.Router();
+const bookNumberController = require('../controller/BookNumber');
 
 const bookController = require('../controller/bookController');
 
 bookRouter.post('/', bookController.createBook);
+bookRouter.post('/add', bookController.addBooks);
 bookRouter.get('/search', bookController.searchBook);
+bookRouter.get('/generate', bookNumberController.generateBookNumber)
 bookRouter.get('/:id', bookController.getBook);
 
 
